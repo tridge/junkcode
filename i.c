792b3960e764b293537775b7fc4f00738b2f14c7
tridge <>
2000-10-09 15:00:21 +0000

    Initial revision

---
+++ i.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+static void print_one(unsigned long long v) 
+{
+	printf("%llu 0x%llX 0%llo", v, v, v);
+	if (v < 256 && isprint(v)) printf(" '%c'", (char)v);
+	printf("\n");
+}
+
+int main(int argc,char *argv[])
+{
+  int i;
+  for (i=1;i<argc;i++) {
+	  char *p;
+	  unsigned long long v = strtoll(argv[i], &p, 0);
+	  if (p == argv[i]) for (; *p ; p++) print_one(*p);
+	  else print_one(v);
+  }
+  return 0;
+}
