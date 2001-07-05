edb3d58a8a4b0b9dc15070224b7b776415dc2bf7
tridge <>
2001-07-05 14:45:14 +0000

    fixed unsigned/signed bug

--- i.c
+++ i.c
@@ -6,7 +6,7 @@
 static void print_one(unsigned long long v) 
 {
 	printf("%llu 0x%llX 0%llo", v, v, v);
-	if (v < 256 && isprint(v)) printf(" '%c'", (char)v);
+	if (v < 256 && isprint(v)) printf(" '%c'", (unsigned char)v);
 	printf("\n");
 }
 
@@ -14,9 +14,9 @@ int main(int argc,char *argv[])
 {
 	int i;
 	for (i=1;i<argc;i++) {
-		char *p;
-		unsigned long long v = strtoll(argv[i], &p, 0);
-		if (p == argv[i]) for (; *p ; p++) print_one(*p);
+		unsigned char *p;
+		unsigned long long v = strtoll(argv[i], (char **)&p, 0);
+		if (p == (unsigned char *)argv[i]) for (; *p ; p++) print_one(*p);
 		else print_one(v);
 	}
 	return 0;
