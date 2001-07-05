42992d153da531c9b6af0036ab69a974bb038e97
tridge <>
2001-07-05 17:33:41 +0000

    *** empty log message ***

---
+++ charset_test.c
@@ -0,0 +1,142 @@
+#include <stdio.h>
+#include <iconv.h>
+
+#define CVAL(buf,pos) (((unsigned char *)(buf))[pos])
+
+typedef unsigned short uint16;
+#define smb_ucs2_t unsigned short
+#define UNI_UPPER    0x1
+#define UNI_LOWER    0x2
+#define UNI_DIGIT    0x4
+#define UNI_XDIGIT   0x8
+#define UNI_SPACE    0x10
+
+typedef struct {
+	smb_ucs2_t lower;
+	smb_ucs2_t upper;
+	unsigned char flags;
+} smb_unicode_table_t;
+
+smb_unicode_table_t map_table[] = {
+#include "unicode_map_table.h"
+};
+
+static int zero_len;
+static int error_count;
+
+/* check if a converted character contains a null */
+static void check_null(iconv_t cd, smb_ucs2_t uc)
+{
+	char buf[10];
+	int inlen=2, outlen=10;
+	char *p = (char *)&uc;
+	char *q = buf;
+	int i, len;
+
+	iconv(cd, &p, &inlen, &q, &outlen);
+	
+	if (outlen == 10) {
+		zero_len++;
+		return;
+	}
+
+	len = (10-outlen);
+
+	for (i=0;i<len;i++) {
+		if (buf[i] == 0) {
+			printf("NULL at pos %d in ucs2 char %04x\n",
+			       i, uc);
+		}
+	}
+}
+
+/* check if a uppercase or lowercase version of a char is longer than
+   the original */
+static void check_case(iconv_t cd, smb_ucs2_t uc)
+{
+	char buf[10];
+	int inlen=2, outlen=10;
+	char *p = (char *)&uc;
+	char *q = buf;
+	int len1, len2, len3;
+	smb_ucs2_t uc2;
+
+	iconv(cd, &p, &inlen, &q, &outlen);
+	len1 = (10-outlen);
+
+	if (len1 == 0) return;
+
+	uc2 = map_table[uc].upper;
+	p = (char *)&uc2;
+	q = buf;
+	inlen = 2;
+	outlen = 10;
+
+	iconv(cd, &p, &inlen, &q, &outlen);
+	len2 = (10-outlen);
+
+	uc2 = map_table[uc].lower;
+	p = (char *)&uc2;
+	q = buf;
+	inlen = 2;
+	outlen = 10;
+
+	iconv(cd, &p, &inlen, &q, &outlen);
+	len3 = (10-outlen);
+
+	if (len2 > len1 || len3 > len1) {
+		printf("case expansion for ucs2 char %04x (%d/%d/%d)\n",
+		       uc, len1, len2, len3);
+		error_count++;
+	}
+}
+
+
+/* check if a character is C compatible */
+static void check_compat(iconv_t cd, smb_ucs2_t uc)
+{
+	char buf[10];
+	int inlen=2, outlen=10;
+	char *p = &uc;
+	char *q = buf;
+
+	/* only care about 7 bit chars */
+	if (CVAL(&uc, 1) || (CVAL(&uc, 0)&0x80)) return;
+
+	iconv(cd, &p, &inlen, &q, &outlen);
+
+	if (buf[0] != CVAL(&uc, 0)) {
+		printf("ucs2 char %04x not C compatible\n",
+		       uc);
+		error_count++;
+	}
+}
+
+int main(int argc, char *argv[])
+{
+	int i;
+	iconv_t cd;
+
+	cd = iconv_open(argv[1], "UCS2");
+
+	if (cd == (iconv_t)-1) {
+		perror(argv[1]);
+		return -1;
+	}
+
+	iconv(cd, NULL, NULL, NULL, NULL);
+	for (i=1;i<0x10000;i++) {
+		check_null(cd, i);
+		check_case(cd, i);
+		check_compat(cd, i);
+	}
+
+	printf("%d chars convertible\n", 0x10000 - zero_len);
+	if (error_count == 0) {
+		printf("character set %s OK for Samba\n", argv[1]);
+	} else {
+		printf("%d errors in character set\n", error_count);
+		return -1;
+	}
+	return 0;
+}
---
+++ utf8.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <iconv.h>
+
+typedef unsigned short smb_ucs2_t;
+
+#define CVAL(buf,pos) (((unsigned char *)(buf))[pos])
+
+static int ucs2_utf8_encode(unsigned char c[3], smb_ucs2_t uc)
+{
+	unsigned char uc1, uc2;
+	uc1 = CVAL(&uc, 0);
+	uc2 = CVAL(&uc, 1);
+
+	if ((uc2 & 0xf8) == 0xd8) {
+		c[0] = 0xed;
+		c[1] = 0x9f;
+		c[2] = 0xbf;
+		return 3;
+	}
+
+	if (uc2 & 0xf8) {
+		c[0] = 0xe0 | (uc2>>4);
+		c[1] = 0x80 | ((uc2&0xF)<<2) | (uc1>>6);
+		c[2] = 0x80 | (uc1&0x3f);
+		return 3;
+	}
+
+	if (uc2 | (uc1 & 0x80)) {
+		c[0] = 0xc0 | (uc2<<2) | (uc1>>6);
+		c[1] = 0x80 | (uc1&0x3f);
+		return 2;
+	} 
+
+	c[0] = uc1;
+	return 1;
+}
+
+static int ucs2_utf8_decode(unsigned char c[3], smb_ucs2_t *uc)
+{
+	*uc = 0;
+
+	if ((c[0] & 0xf0) == 0xe0) {
+		CVAL(uc, 1) = ((c[0]&0xF)<<4) | ((c[1]>>2)&0xF);
+		CVAL(uc, 0) = (c[1]<<6) | (c[2]&0x3f);
+		return 3;
+	}
+
+	if ((c[0] & 0xe0) == 0xc0) {
+		CVAL(uc, 1) = (c[0]>>2) & 0x7;
+		CVAL(uc, 0) = (c[0]<<6) | (c[1]&0x3f);
+		return 2;
+	}
+
+	CVAL(uc, 0) = c[0];
+	return 1;
+}
+
+
+int main(int argc, char *argv[])
+{
+	unsigned short s[2];
+	int i, j;
+	unsigned char utf8[10];
+	unsigned char foo[10];
+	int len, len2;
+	iconv_t cd;
+
+	s[1] = 0;
+
+	cd = iconv_open(argv[1], "UCS2");
+	
+	for (i=1;i<0x10000;i++) {
+		int inlen, outlen;
+		char *p;
+		char *q;
+
+		p = s;
+		q = utf8;
+		s[0] = i;
+		inlen = 4;
+		outlen = 10;
+		iconv(cd, &p, &inlen, &q, &outlen);
+		len = strlen(utf8);
+
+		len2 = ucs2_encode(foo, i);
+
+		if (ucs2_decode(utf8) != i) {
+			printf("%04x %04x\n", i, ucs2_decode(utf8));
+		}
+
+		if (len != len2 || strncmp(foo, utf8, len) != 0) {
+			printf("%02x: ", i);
+			for (j=0;j<len;j++) {
+				printf("%02x ", utf8[j]);
+			}
+			printf("\n");
+			
+			printf("%02x: ", i);
+			for (j=0;j<len2;j++) {
+				printf("%02x ", foo[j]);
+			}
+			printf("\n");
+		}
+	}
+}
