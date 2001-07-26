629cd07ce272f754fb15a3863c6da3a159e824b5
tridge <>
2001-07-26 19:36:46 +0000

    added support for minor devices

--- trd/trd.c
+++ trd/trd.c
@@ -5,6 +5,8 @@
 
 #define MAJOR_NR   240
 
+#define MAX_DEVS 32
+
 #define DEVICE_NAME "trd"
 #define DEVICE_REQUEST do_trd_request
 #define DEVICE_NR(device) (MINOR(device))
@@ -24,7 +26,7 @@
 #define TRD_SIZE (trd_size<<10)
 
 static int trd_blocksizes[1] = {TRD_BLOCK_SIZE};
-static void **trd_base;
+static void **trd_base[MAX_DEVS];
 static int trd_chunks;
 static unsigned trd_size = 4096;
 
@@ -34,11 +36,12 @@ static void do_trd_request(request_queue_t * q)
 {
 	u_long start, ofs, len, len1;
 	void *addr;
-	int chunk;
+	int chunk, minor;
 
 	while (1) {
 		INIT_REQUEST;
-	    
+
+		minor = MINOR(CURRENT->rq_dev);
 		start = CURRENT->sector << 9;
 		len  = CURRENT->current_nr_sectors << 9;
 		
@@ -59,7 +62,7 @@ static void do_trd_request(request_queue_t * q)
 		while (len) {
 			chunk = start / TRD_CHUNK_SIZE;
 			ofs = start % TRD_CHUNK_SIZE;
-			addr = (void *) (((char *)(trd_base[chunk])) + ofs);
+			addr = (void *) (((char *)(trd_base[minor][chunk])) + ofs);
 
 			len1 = len;
 			if (ofs + len1 > TRD_CHUNK_SIZE) {
@@ -80,11 +83,50 @@ static void do_trd_request(request_queue_t * q)
 	}
 }
 
+static int trd_allocate(int minor)
+{
+	int i;
+
+	/* it might be already allocated */
+	if (trd_base[minor]) return 0;
+
+	trd_base[minor] = (void **)vmalloc(sizeof(void *)*trd_chunks);
+	if (!trd_base[minor]) goto nomem;
+	memset(trd_base[minor], 0, sizeof(void *)*trd_chunks);
+
+	for (i=0;i<trd_chunks-1;i++) {
+		trd_base[minor][i] = vmalloc(TRD_CHUNK_SIZE);
+		if (!trd_base[minor][i]) goto nomem;
+	}
+	trd_base[minor][i] = vmalloc(TRD_SIZE - (i-1)*TRD_CHUNK_SIZE);
+	if (!trd_base[minor][i]) goto nomem;
+
+	printk(KERN_INFO DEVICE_NAME ": Allocated %dk to minor %d\n",
+	       TRD_SIZE>>10, minor);
+
+	return 0;
+
+nomem:
+	if (trd_base[minor]) {
+		for (i=0;i<trd_chunks;i++) {
+			if (trd_base[minor][i]) vfree(trd_base[minor][i]);
+		}
+		vfree(trd_base[minor]);
+		trd_base[minor] = NULL;
+	}	
+
+	printk(KERN_ERR DEVICE_NAME ": Unable to allocate trd of size %d\n",
+	       TRD_SIZE);
+	return -ENOMEM;
+}
+
+
 static int trd_open(struct inode *inode, struct file *filp)
 {
-	if (trd_base == NULL) return -ENOMEM;
+	int ret;
 
-	printk(KERN_DEBUG DEVICE_NAME ": trd opened\n");
+	ret = trd_allocate(MINOR(inode->i_rdev));
+	if (ret != 0) return ret;
 
 	MOD_INC_USE_COUNT;
 
@@ -94,7 +136,6 @@ static int trd_open(struct inode *inode, struct file *filp)
 static int trd_release(struct inode *inode, struct file *filp)
 {
 	MOD_DEC_USE_COUNT;
-	printk(KERN_DEBUG DEVICE_NAME ": trd released\n");
 	return 0;     
 }
 
@@ -120,21 +161,10 @@ static struct block_device_operations trd_fops =
 	ioctl:		trd_ioctl,
 };
 
+
 static int trd_init(void)
 {
-	int i;
-
 	trd_chunks = (TRD_SIZE + (TRD_CHUNK_SIZE-1)) / TRD_CHUNK_SIZE;
-	trd_base = (void **)vmalloc(sizeof(void *)*trd_chunks);
-	if (!trd_base) goto nomem;
-	memset(trd_base, 0, sizeof(void *)*trd_chunks);
-
-	for (i=0;i<trd_chunks-1;i++) {
-		trd_base[i] = vmalloc(TRD_CHUNK_SIZE);
-		if (!trd_base[i]) goto nomem;
-	}
-	trd_base[i] = vmalloc(TRD_SIZE - (i-1)*TRD_CHUNK_SIZE);
-	if (!trd_base[i]) goto nomem;
 
 	if (register_blkdev(MAJOR_NR, DEVICE_NAME, &trd_fops)) {
 		printk(KERN_ERR DEVICE_NAME ": Unable to register_blkdev(%d)\n", MAJOR_NR);
@@ -148,32 +178,22 @@ static int trd_init(void)
 	printk(KERN_DEBUG DEVICE_NAME ": trd initialised size=%d\n", TRD_SIZE);
 
 	return 0;
-
-nomem:
-	if (trd_base) {
-		for (i=0;i<trd_chunks;i++) {
-			if (trd_base[i]) vfree(trd_base[i]);
-		}
-		vfree(trd_base);
-		trd_base = NULL;
-	}	
-
-	printk(KERN_ERR DEVICE_NAME ": Unable to allocate trd of size %d\n",
-	       TRD_SIZE);
-	return -ENOMEM;
 }
 
 static void __exit trd_cleanup(void)
 {
-	int i;
+	int minor, i;
 
 	unregister_blkdev(MAJOR_NR, DEVICE_NAME);
 
-	for (i=0;i<trd_chunks;i++) {
-		vfree(trd_base[i]);
+	for (minor=0;minor<MAX_DEVS;minor++) {
+		if (!trd_base[minor]) continue;
+		for (i=0;i<trd_chunks;i++) {
+			vfree(trd_base[minor][i]);
+		}
+		vfree(trd_base[minor]);
+		trd_base[minor] = NULL;
 	}
-	vfree(trd_base);
-	trd_base = NULL;
 	printk(KERN_DEBUG DEVICE_NAME ": trd released\n");
 }
 
