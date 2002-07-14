e177428f6bae3970c59dd278001052cbc5363ee6
tridge <>
2002-07-14 01:47:21 +0000

    a fast fn for getting the list of groups for a user

---
+++ getgrouplist.c
@@ -0,0 +1,70 @@
+#define NGROUPS_MAX 1000
+
+#include <stdio.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <string.h>
+#include <grp.h>
+#include <pwd.h>
+
+
+/*
+  This is a *much* faster way of getting the list of groups for a user
+  without changing the current supplemenrary group list. The old
+  method user getgrent() which could take 20 minutes on a really big
+  network with hundeds of thousands of groups and users. The new method
+  takes a couple of seconds. (tridge@samba.org)
+  */
+int
+getgrouplist(uname, agroup, groups, grpcnt)
+	const char *uname;
+	gid_t agroup;
+	register gid_t *groups;
+	int *grpcnt;
+{
+	gid_t gids_saved[NGROUPS_MAX + 1];
+	int ret, ngrp_saved;
+	
+	ngrp_saved = getgroups(NGROUPS_MAX, gids_saved);
+	if (ngrp_saved == -1) {
+		return -1;
+	}
+
+	if (initgroups(uname, agroup) != 0) {
+		return -1;
+	}
+
+	ret = getgroups(NGROUPS_MAX, groups);
+	if (ret >= 0) {
+		*grpcnt = ret;
+	}
+
+	if (setgroups(ngrp_saved, gids_saved) != 0) {
+		/* yikes! */
+		fprintf(stderr,"getgrouplist: failed to reset group list!\n");
+		exit(1);
+	}
+
+	return ret;
+}
+
+int main(int argc, char *argv[])
+{
+	char *user = argv[1];
+	struct passwd *pwd;
+	gid_t gids[NGROUPS_MAX + 1];
+	int count, ret, i;
+
+	pwd = getpwnam(user);
+
+	ret = getgrouplist(user, pwd->pw_gid, gids, &count);
+
+	printf("Got %d groups\n", ret);
+	for (i=0;i<count;i++) {
+		printf("%u ", (unsigned)gids[i]);
+	}
+	printf("\n");
+	
+	return 0;
+}
