449e6217ad8f3c47f9169eb840b2dc45d4343712
tridge <>
2002-11-12 22:10:36 +0000

    useful util

---
+++ listgroups.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <grp.h>
+#include <pwd.h>
+#include <sys/types.h>
+
+int main(void)
+{
+	struct group *grp;
+	int ngroups, i;
+	gid_t *grps;
+
+	ngroups = getgroups(0, NULL);
+
+	grps = (gid_t *)malloc(ngroups * sizeof(gid_t));
+	
+	getgroups(ngroups, grps);
+
+	grp = getgrgid(getegid());
+	printf("%5d %s (primary)\n", getegid(), grp->gr_name);
+
+	for (i=0;i<ngroups;i++) {
+		grp = getgrgid(grps[i]);
+		printf("%5d %s\n", grps[i], grp->gr_name);
+	}
+	return 0;
+}
