dab648c5fc9cd0acec2662b93053caf305b77eea
Andrew Tridgell <tridge@samba.org>
2007-07-07 11:06:05 +1000

    added some comments

--- alloc_mmap/alloc_mmap.c
+++ alloc_mmap/alloc_mmap.c
@@ -1,7 +1,33 @@
 /*
   a mmap based memory allocator
 
-  tridge@samba.org, July 2007  
+   Copyright (C) Andrew Tridgell 2007
+   
+   This library is free software; you can redistribute it and/or
+   modify it under the terms of the GNU Lesser General Public
+   License as published by the Free Software Foundation; either
+   version 2 of the License, or (at your option) any later version.
+
+   This library is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+   Lesser General Public License for more details.
+
+   You should have received a copy of the GNU Lesser General Public
+   License along with this library; if not, write to the Free Software
+   Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+/*
+
+  The basic aims of this allocator are:
+
+     - fast
+     - releases memory back to the OS as much as possible
+     - no per-block prefix (so extremely low overhead)
+     - no safety checking at all
+     - no attempt at thread safety. Threads will cause crashes
+
+  Currently it does not return aligned memory
  */
 #include <stdio.h>
 #include <stdint.h>
@@ -271,7 +297,7 @@ static void alloc_free(void *ptr)
 
 	ph->elements_used--;
 
-	if (ph->elements_used == 0) {
+	if (unlikely(ph->elements_used == 0)) {
 		MOVE_LIST(bs->page_list, state.empty_list, ph);
 		state.num_empty_pages++;
 		if (state.num_empty_pages == FREE_PAGE_LIMIT) {
@@ -386,4 +412,5 @@ void *realloc(void *ptr, size_t size)
 {
 	return alloc_realloc(ptr, size);
 }
+
 #endif
