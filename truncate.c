0d4888689aacdaa7745ffb4c7d03a0de7e5bd94c
Andrew Tridgell <tridge@samba.org>
2008-10-28 21:25:47 +1100

    allow size set

--- truncate.c
+++ truncate.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 #include <fcntl.h>
 
 int main(int argc, char *argv[])
 {
 	int fd;
 	fd = open(argv[1], O_RDWR|O_CREAT|O_TRUNC, 0600);
-	ftruncate(fd, 1);
+	ftruncate(fd, strtoull(argv[2], NULL, 0));
 	close(fd);
+	return 0;
 }
