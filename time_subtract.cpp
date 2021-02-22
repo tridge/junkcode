420c2044f3b7d942bd454ef8ec3490a472087a15
Andrew Tridgell <andrew@tridgell.net>
2021-02-22 15:58:28 +1100

    time: more time subtraction games

--- time_subtract.cpp
+++ time_subtract.cpp
@@ -1,6 +1,33 @@
 #include <stdio.h>
 #include <stdint.h>
 
+/*
+  subtracting two 16 bit timestamps must cast the result to uint16_t
+  or it does not handle wrap correctly. Using this inline function
+  produces the correct result
+ */
+static inline uint16_t time_diff16(uint16_t curr_t, uint16_t prev_t)
+{
+    return uint16_t(curr_t - prev_t);
+}
+
+/*
+  subtracting two 16 bit timestamps must cast the result to uint16_t
+  or it does not handle wrap correctly. Using this inline function
+  produces the correct result
+ */
+static inline uint32_t time_diff32(uint32_t curr_t, uint32_t prev_t)
+{
+    uint32_t delta = curr_t - prev_t;
+    if (delta > 0xF0000000U) {
+        // very likely a bug, subtracting a newer timestamp from an
+        // older one
+    }
+    return delta;
+}
+
+
+
 int main(void)
 {
     uint16_t x1 = 0;
@@ -16,6 +43,9 @@ int main(void)
         }
         if (uint16_t(x2 - x1) != 10) {
             printf("bad wrap3\n");
+        }
+        if (time_diff16(x2, x1) != 10) {
+            printf("bad wrap4\n");
         }
     }
     return 0;
