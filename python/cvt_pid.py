ee53176d753795601a358dc973f6d40227b94c4c
Andrew Tridgell <andrew@tridgell.net>
2022-05-05 14:04:39 +1000

    small pid fix

--- python/cvt_pid.py
+++ python/cvt_pid.py
@@ -11,8 +11,14 @@ I = float(sys.argv[2])
 D = float(sys.argv[3])
 tau = float(sys.argv[4])
 
-print(P,I,D,tau)
+print('Old PID: P=%.2f I=%.2f D=%.2f TCONST=%.2f' % (P,I,D,tau))
 
 kp_ff = max((P - I * tau) * tau  - D, 0)
 
-print("FF=%f" % kp_ff)
+print("RATE_FF=%.2f" % kp_ff)
+print("RATE_P=%.2f" % D)
+print("RATE_I=%.2f" % (I*tau))
+print("RATE_D=0.00")
+print("TCONST=%.2f" % tau)
+
+
